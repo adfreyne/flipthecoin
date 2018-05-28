9fb4968a308bfff7db622789f649524806c13a47
Adriaan De Freyne <adriaandefreyne@gmail.com>
2018-05-28 12:11:54 +0200

    instore handelSubmit and mapStateToProps

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import FlipFormComponent from './FlipFormComponent';
+import { connect } from 'react-redux'
+import { restartGame } from './store/flip';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(restartGame())
+  }
   render() {
+    const { bets } = this.props
     return (
       <div >
         test
         <form>
           <button >RESTART GAME</button>
           <FlipFormComponent />
+          <ul>
+            {bets.map((b, index) => (
+              <li key={index}>
+                {b}
+              </li>
+            ))}
+          </ul>
         </form>
       </div>
     );
   }
 }
-
-export default App;
+const mapStateToProps = (state) => ({
+  flipResult: state.flip.flipResult,
+  bets: state.flip.bets
+})
+export default connect(mapStateToProps)(App)
--- src/FlipFormComponent.js
+++ src/FlipFormComponent.js
@@ -1,10 +1,13 @@
 import React, { PureComponent } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import { play } from './store/flip'
 
 class FlipFormComponent extends PureComponent {
   render() {
+    const { handleSubmit } = this.props
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <Field name='bet' component='input' type='number' />
         <button type='submit'>HEADS</button>
         <button type='submit'>TAILS</button>
@@ -12,5 +15,11 @@ class FlipFormComponent extends PureComponent {
     )
   }
 }
-
-export default reduxForm({ form: 'bet' })(FlipFormComponent)
+const onSubmit = ({ bet }, dispatch) => {
+  dispatch(play(bet))
+  console.log(bet)
+}
+export default reduxForm({
+  form: 'bet',
+  onSubmit,
+})(FlipFormComponent)
